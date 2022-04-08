11e3517390643d9f44d00402b9b21bd134088a4f
Michael Lok <michael91lok@gmail.com>
2022-04-07 20:26:14 -0400

    reticulok

--- setup.py
+++ setup.py
@@ -1,22 +1,7 @@
-import os
 from setuptools import setup
 
 
-def get_long_description():
-    with open(
-        os.path.join(os.path.dirname(__file__), "README.md"),
-        encoding="utf8",
-    ) as fp:
-        return fp.read()
-
-
-setup(
-    name="reticulok",
-    version="0.1.1",
-    long_description=get_long_description(),
-    long_description_content_type="text/markdown",
-    description="Python utilities package",
-    author="Michael Lok",
-    url="https://github.com/michael-lok",
-    license="GNU General Public License v3.0"
-)
+if __name__ == "__main__":
+    setup(
+        version="0.0.1"
+    )
