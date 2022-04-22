e216b1c95a97da3f1c61f69b1b5821cc9888506b
Michael Lok <michael91lok@gmail.com>
2022-04-21 20:49:19 -0400

    init miscellaneous

---
+++ reticulok/miscellaneous/books_sold.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+def get_nth_highest_sold(
+    books: List[tuple[float, int]], n: int
+    ) -> tuple[float, int]:
+    """
+    Find the nth highest sold book from a list of books. The first
+    element in each tuple represents the price; the second represents
+    the number of units sold. If there is a tie in frequency sold,
+    then return the book that is the least expensive amongst the tied
+    records.
+
+    Args:
+        books (list): array of books, with price and units sold
+        n (int): nth highest book sold to be returned
+
+    Returns:
+        tuple: book price and units sold
+    """
+    if not books:
+        return None
+
+    try:    
+        nth_sold = sorted(list(set([x[1] for x in books])), reverse=True)[n-1]
+    except IndexError: # input n is out of range
+        return None
+
+    matches = [i for i, x in enumerate(books) if x[1] == nth_sold]
+
+    if len(matches) == 1:
+        return books[matches[0]]
+    else:
+        cheapest = min([x[0] for i, x in enumerate(books) if i in matches])
+        return [(x, y) for x, y in books if x == cheapest and y == nth_sold][0]
